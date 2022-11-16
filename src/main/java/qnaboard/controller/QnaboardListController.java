27f5a0ddb88d86d1d740bfcbe6959ba1da89b17e
jh223 <qazwsx8767@naver.com>
2022-11-16 14:34:59 +0900

    qna 경로 설정 박지현
    (cherry picked from commit 8f6e61e9c7b8816ad600fb52e604ccec67a0c7ee)

--- src/main/java/qnaboard/controller/QnaboardListController.java
+++ src/main/java/qnaboard/controller/QnaboardListController.java
@@ -1,8 +1,16 @@
 package qnaboard.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class QnaboardListController {
-
+	@RequestMapping("qna.qna")
+	public String main(HttpServletRequest request, Model model) {
+			
+	      return "/qna";
+	}
 }
