d8c801c300bfe4fd3e5633824226377cf255bbab
DESKTOP-SPO2P5A\user <jihyun000304@gmail.com>
2022-11-17 15:00:56 +0900

    메인-업체리스트_양지
    (cherry picked from commit d5dc0d962b63573654fa9cc0a043314ddaaa8974)

--- src/main/java/foodstore/controller/MainController.java
+++ src/main/java/foodstore/controller/MainController.java
@@ -1,17 +1,34 @@
 package foodstore.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import foodstore.model.FoodstoreBean;
+import foodstore.model.FoodstoreDAO;
+
 @Controller
 public class MainController {
 	
+	@Autowired
+	private FoodstoreDAO fdao;
+	
 	@RequestMapping("main.fs")
 	 public String main(HttpServletRequest request, Model model) {
-			
+		
+		//업체 리스트 불러오기
+		List<FoodstoreBean> lists = fdao.getAllFoodStore();
+
+		String fullpath = request.getContextPath()+"/resources/fs_images";
+
+		model.addAttribute("lists",lists);
+		model.addAttribute("fullpath",fullpath);
+		
 	      return "/main";
 	   }
 	
