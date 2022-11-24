ea9d3e55523e552bd98f404e83c4feb305a11196
ramivo08 <hboram8@naver.com>
2022-11-24 09:38:45 +0900

    관리자 상세보기 임시커밋, 수정 주소api 완료 -보람

--- src/main/java/foodstore/model/FoodstoreDAO.java
+++ src/main/java/foodstore/model/FoodstoreDAO.java
@@ -29,6 +29,37 @@ public class FoodstoreDAO {
 		return cnt;
 	}
 
+	//상세보기, 수정폼 띄우기, 삭제할때 번호가져오기
+	public FoodstoreBean selectFoodstore(String store_no) {
+		
+		FoodstoreBean fsbean;
+		
+		fsbean = sqlSessionTemplate.selectOne(namespace+".SelectFoodstore", store_no);
+		System.out.println("fsb.getStore_no() :"+ fsbean.getStore_no());
+		return fsbean;
+	}
+
+	
+	//업체 수정하기
+	public int updateFoodstore(FoodstoreBean foodstore) {
+		
+		int cnt= -1;
+		cnt = sqlSessionTemplate.update(namespace+".UpdateFoodstore", foodstore);
+		
+		System.out.println("updateFoodstore: "+cnt);
+		
+		return cnt;
+	}
+
+	public int deleteFoodstore(String store_no) {
+		
+		int cnt =-1;
+		cnt = sqlSessionTemplate.delete(namespace+".DeleteFoodstore",store_no);
+		System.out.println("deleteFoodstore: "+cnt);
+		
+		return cnt;
+	}
+
 
 
 }
